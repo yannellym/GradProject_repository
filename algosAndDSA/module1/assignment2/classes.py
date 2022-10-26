840abc89fa10d7e1c2e74cca8e566aa8b1eb36e3
yannelly25 <yannellym@gmail.com>
2022-10-26 11:57:41 -0400

    restructured code

--- algosAndDSA/module1/assignment2/classes.py
+++ algosAndDSA/module1/assignment2/classes.py
@@ -7,20 +7,108 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
-    def visualize(self):
+    def setList(self, store):
+        # Looks through each item in the store and assigns it to a node in the linked list
+        first_node = Node(store[0])
+        second_node = Node(store[1])
+        third_node = Node(store[2])
+        fourth_node = Node(store[3])
+        fifth_node = Node(store[4])
+        sixth_node = Node(store[5])
+        seventh_node = Node(store[6])
+        eigth_node = Node(store[7])
+        nineth_node = Node(store[8])
+        tenth_node = Node(store[9])
+        eleventh_node = Node(store[10])
+        twelve_node = Node(store[11])
+        thirteen_node = Node(store[12])
+        fourteen_node = Node(store[13])
+        fifteen_node = Node(store[14])
+        sixteen_node = Node(store[15])
+        # assigns first node to the head of the list
+        self.head = first_node
+        #assigns rest of the nodes to connect to each other
+        first_node.next = second_node
+        second_node.next = third_node
+        third_node.next = fourth_node
+        fourth_node.next = fifth_node 
+        fifth_node.next = sixth_node
+        sixth_node.next = seventh_node 
+        seventh_node.next = eigth_node
+        eigth_node.next = nineth_node 
+        nineth_node.next = tenth_node
+        tenth_node.next = eleventh_node     
+        eleventh_node.next = twelve_node
+        twelve_node.next = thirteen_node 
+        thirteen_node.next = fourteen_node
+        fourteen_node.next = fifteen_node 
+        fifteen_node.next = sixteen_node
+        sixteen_node.next = None 
+        
+    def visualizeList(self):
+        '''Prints out the current list of nodes to help visualize it.'''
         node = self.head
-        nodes = []
+        nodes = ""
         while node is not None:
-            nodes.append(node.data)
+            nodes += str(node.data)
+            nodes += '->'
             node = node.next
-        nodes.append("None")
-        print(" -> ".join(nodes))
+        nodes+= "None"
+        print(nodes)
     
-    def __iter__(self):
-        # iterates through the list as long as the node is not None
-        node = self.head
-        while node is not None:
-            # yield is used to produce a sequence of values, unliked return
-            # return only returns a specified value
-            yield node
-            node = node.next 
+    def replaceHead(self, current):
+        '''Replaces the head of the list if the head equals the target.'''
+        next = current.next
+        current.next = None
+        self.head = next
+        return self.head
+
+    def addNode(self, current):
+        current = current.next
+                
+    def removeNode(self, prev, current):
+        prev.next = current.next
+        print("Node was removed from the list")
+      
+    
+    def removeOrInsert(self, target):
+        prev = None
+        current = self.head
+        
+        # handles the edge case where the target node would be the first node
+        if current is None or current.data == target:
+            self.replaceHead(current) 
+            print("** Replaced head **")
+            self.visualizeList()
+            return 
+        
+        while current:       
+            # if the current node equals the target node, remove it    
+            if current.data == target: # works
+                self.removeNode(prev, current)
+                # print the current linked list
+                self.visualizeList()
+                return
+            if current.next:
+                while current.next.data < target:
+                    current = current.next
+                newNode = Node(target)
+                newNode.next = current.next
+                current.next = newNode
+                print("New node was added to the list")
+                self.visualizeList()
+                return self.head
+                
+            
+            prev = current
+            current = current.next
+           
+                
+            print("new iter")
+        
+            
+            
+    
+            
+        
+        
--- algosAndDSA/module1/assignment2/main.py
+++ algosAndDSA/module1/assignment2/main.py
@@ -20,14 +20,14 @@ def main():
     # references the linked list
     L = LinkedList()
     # sets and connects the nodes in the linked list
-    setList(L, store)
+    L.setList(store)
     # asks users to input a number and sets this as the target
     target = int(input("please input number to check in the linked list: \n"))
     # removes or inserts the target node in the linked list
-    removeOrInsert(L, target)
+    L.removeOrInsert(target)
     
 
-# Helper functions for main
+# Helper functions for main11
 def lookThroughFile(store): 
     # reads a list of Integer numbers from a file named data.txt
     with open('algosAndDSA/module1/assignment2/data.txt', 'r') as f:
@@ -36,75 +36,35 @@ def lookThroughFile(store):
             store.append(int(line.strip()))
         # sort the store list
         store.sort()
-    
-def setList(l_list, store):
-    # Looks through each item in the store and assigns it to a node in the linked list
-    first_node = Node(store[0])
-    second_node = Node(store[1])
-    third_node = Node(store[2])
-    fourth_node = Node(store[3])
-    fifth_node = Node(store[4])
-    sixth_node = Node(store[5])
-    seventh_node = Node(store[6])
-    eigth_node = Node(store[7])
-    nineth_node = Node(store[8])
-    tenth_node = Node(store[9])
-    eleventh_node = Node(store[10])
-    twelve_node = Node(store[11])
-    thirteen_node = Node(store[12])
-    fourteen_node = Node(store[13])
-    fifteen_node = Node(store[14])
-    sixteen_node = Node(store[15])
-    # assigns first node to the head of the list
-    l_list.head = first_node
-    #assigns rest of the nodes to connect to each other
-    first_node.next = second_node
-    second_node.next = third_node
-    third_node.next = fourth_node
-    fourth_node.next = fifth_node 
-    fifth_node.next = sixth_node
-    sixth_node.next = seventh_node 
-    seventh_node.next = eigth_node
-    eigth_node.next = nineth_node 
-    nineth_node.next = tenth_node
-    tenth_node.next = eleventh_node     
-    eleventh_node.next = twelve_node
-    twelve_node.next = thirteen_node 
-    thirteen_node.next = fourteen_node
-    fourteen_node.next = fifteen_node 
-    fifteen_node.next = sixteen_node
-    sixteen_node.next = None 
-
-def removeOrInsert(l_list, target):
-    # remove node if already in linked list
-    prev = None
-    current = l_list.head
-    while current:
-        if current.data == target:
-            prev.next = current.next
-            print("Node was removed from the list")
-            break
-        prev = current
-        current = current.next
-    for node in l_list:
-        print(node.data)  
-        
-    current = l_list.head
-    
-    # handles the edge case where the target node would be the first node
-    if current is None or current.data >= target:
-        newNode = Node(target)
-        newNode.next = l_list.head
-        head = newNode
-        return head
-    
-    while current.next and current.next.data < target:
-        current = current.next
-    newNode = Node(target)
-    newNode.next = current.next
-    current.next = newNode
-    print("New node was added to the list")
-    for node in l_list:
-        print(node.data)
 
 main()
+
+#   prev = None
+#     current = l_list.head
+#     while current:
+#         if current.data == target:
+#             prev.next = current.next
+#             print("Node was removed from the list")
+#             break
+#         prev = current
+#         current = current.next
+#     for node in l_list:
+#         print(node.data)  
+        
+#     current = l_list.head
+    
+#     # handles the edge case where the target node would be the first node
+#     if current is None or current.data >= target:
+#         newNode = Node(target)
+#         newNode.next = l_list.head
+#         head = newNode
+#         return head
+    
+#     while current.next and current.next.data < target:
+#         current = current.next
+#     newNode = Node(target)
+#     newNode.next = current.next
+#     current.next = newNode
+#     print("New node was added to the list")
+#     for node in l_list:
+#         print(node.data)
