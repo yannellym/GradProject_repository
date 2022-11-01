2d0decf03d1d5dd9f4f49765e7004a9204c97f8f
yannelly25 <yannellym@gmail.com>
2022-11-01 10:43:03 -0400

    reformatting code

--- algosAndDSA/module2/main.py
+++ algosAndDSA/module2/main.py
@@ -1,19 +1,16 @@
+from classes import Stack
 '''
 Create a program that reads a list of people (use the class Person seem in class) from a file named people.csv (create your own file with about 15 fictional people).
 Store those people in a stack (use any stack implementation you want among the ones presented in class). Ask repeatedly the user integer numbers from 1 to 4, and according to the user informed number pop this amount of people, printing the name of the one remaining in the top of the stack. For example, if the user enters 2, removes two people from the stack and print the "third" remaining in the stack
 The program stops as the stack becomes empty.
 '''
-# list to store contents of people.csv
-store = []
 
 def main():
+    store = Stack()
     # looks through file and adds contents to store
-    lookThroughFile(store)
-    removeFromStack()
+    lookThroughFile(store.list)
+    store.takeAction()
         
-
-
-
 
 # Helper functions for main
 def lookThroughFile(store): 
@@ -22,61 +19,8 @@ def lookThroughFile(store):
         # for each line in  people.csv, append it to the store list without any trailing spaces
         for line in f:
             store.append(line.strip())
-        print("adding file contents to the array...")
+        print("adding file contents to the stack...")
     print(store)
 
-def askUser():
-    '''
-    Ask the user for to enter a number between 1 and 4. If The user doesn't enter an 
-    integer, or the integer is not between 1-4, keep asking the user for a valid input
-    Return:
-    - The number inputted by the user as an INT
-    '''
-    number_chosen = input("Please choose a number between 1 and 4: \n")
-    # if it is not an instance of INT, ask user again for a number.
-    if not number_chosen.isdigit():
-        print("Number must be an integer")
-        askUser()
-        
-    elif int(number_chosen) > 4 or int(number_chosen) < 1:
-        print("Number must be between 1 and 4.")
-        askUser()
-        
-   
-    print("returning an integer", number_chosen)
-    return int(number_chosen)
-    
-def popItem(chosen_num):
-    '''Will receive an amount of users to pop from the list
-    Parameters:
-    - number_chosen: INT number of users to pop from the list
-    Return:
-    
-    '''
-    global store
-    
-    if chosen_num > len(store):
-        print(f"Current stack length is {len(store)}. Please choose a different number to remove")
-        
-    
-    elif chosen_num == len(store):
-        store = []
-        print(f"Stack is now empty: {store} ")
-        return False
-    else: 
-        if chosen_num < len(store):
-            for i in range(chosen_num):
-                store.pop()
-        print(f"New stack is: {store} ")
-    return True
-    
-        
-def removeFromStack():
-    # asks user for a number and sets it in a variabled called chosen_number
-    chosen_num = askUser()
-    continue_pop = popItem(chosen_num)
-    while continue_pop:
-        removeFromStack()
-        break
     
 main()
