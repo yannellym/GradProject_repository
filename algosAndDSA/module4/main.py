c3da95ab15ff03ec143509226b3d6b5f9bca1a12
yannelly25 <yannellym@gmail.com>
2022-11-16 18:12:22 -0500

    draft 2 module 4

--- algosAndDSA/module4/main.py
+++ algosAndDSA/module4/main.py
@@ -19,11 +19,27 @@ and should return the nxn product matrix using a return statement.
 '''
 
 def main():
+    # problem 1
     arr_store = [[20, 21, 25, 28, 33, 34, 35, 36, 41, 42],[18, 54, 76, 81, 36, 48, 99]]
     nums = [7, 9]
+    # Runs a loop ith times. With i being the length of the arr_store.
     for i in range(len(arr_store)):
+        # passes the arr in position i of the arr_store and the number in position i of nums
+        # prints the return statement from the divisible_by function
+        print("**Problem 1 answer:")
         print(divisible_by(arr_store[i], nums[i]))
-
+        
+    # problem 2
+    num_arr = [[50, 120, 250, 100, 20, 300, 200],[12.4, 45.9, 8.1, 79.8, -13.64, 5.09]]
+    # Runs a loop ith times. With i being the length of the arr_store.
+    for i in range(len(num_arr)):
+        # passes the arr in position i of the arr_store and the number in position i of nums
+        # prints the return statement from the divisible_by function
+        print("**Problem 2 answer:")
+        print(smallest_gap(num_arr[i]))
+        
+    # problem 3
+    
 
 def divisible_by(arr, n):
     '''
@@ -36,14 +52,41 @@ def divisible_by(arr, n):
     
     Return:
     - count: the counto of how many digits are divisible by n. 
-    
     '''
     count = 0
     for digit in arr:
         if digit % n == 0:
             count += 1
-    return count     
+    return count   
 
+def smallest_gap(num_arr):
+    '''
+    Parameters:
+    - num_arr: an array of numbers type INT, and FLOAT
+    
+    Variables:
+    i , j : INT will be used to loop through the array and compare numbers to each other.
+    gap : a variable set to positive infinity. 
+    
+    Function will look through each number in the array and comparate the ith element with the jth element. 
+    It will subtract j - i and set it to its absolute value. Then, if the value is less than the variable "gap",
+    it will set gap to equal that new value.
+    
+    Return:
+    - gap: the smallest gap  
+    '''
+    i = 0
+    j = 1
+    gap = float('inf')
+    
+    for j in range(1,len(num_arr)):
+        gap = min(gap, abs(num_arr[j] - num_arr[i]))
+        i += 1
+    return gap
+     
+        
+          
+          
 main()
 
 
