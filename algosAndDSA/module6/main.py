3ef85017cf540101d3a9108d6735eec17a8d9d99
yannelly25 <yannellym@gmail.com>
2022-11-29 18:28:03 -0500

    working on part 2

--- algosAndDSA/module6/main.py
+++ algosAndDSA/module6/main.py
@@ -1,5 +1,3 @@
-import math
-
 def main():
     # 1) a) Write python code for a recursive algorithm that will calculate the number of digits in the 
     # binary expansion/representation of a positive integer n. The logic of the recursive algorithm 
@@ -14,28 +12,67 @@ def main():
     # creating the binary expansion of n. 
     # Be sure to include comments identifying the input and output for this algorithm, as well as 
     # comments explaining what is accomplished by the key steps of the algorithm.   
-    
     def getbinary(number):
+        '''
+        Function: calculates the number of digits in the binary expansion/representation of a positive integer n.
+        Input: A positive integer number.
+        Output: A digit representing the number of digits in the binary expansion of n.
+        '''
         # Base case for the function
-        if number == 0:
+        if number == 0:   # if the number is 0, returns 0
             return 0
         # Recursive call
-        # bin_num is where we will save the results
-        bin_num = getbinary(number // 2)
-        
-        final = (number % 2 + 10 * bin_num)
-        print(len(str(final)))
-        return final
-        # return len(str(final))
+        # this will add 1 everytime the function is called
+        # it serves as a counter for the function
+        return getbinary(number // 2) + 1  
+    
    
     # functions to be executed
     
-    # problem 1
+    # 1) b) 
         # decimal 1 = 1
         # decimal = 131 binary = 10000011
         # decimal = 206 binary = 11001110
         # decimal = 192 binary = 11000000
-    decimal_number = 203
+        # Example 1:
+            # decimal = 203 binary = 11001011
+            # if n > 1, the answer is 1 more than the number of digits in the binary representation of n/2. 
+            # - > 206 /2 = 103
+            # decimal = 103 binary  = 1100111
+            # 11001011 len = 8  and 1100111 len = 7
+        # Example 2:
+            # decimal = 341 binary = 101010101
+            # if n > 1, the answer is 1 more than the number of digits in the binary representation of n/2. 
+            # - > 341 // 2 = 170
+            # decimal = 170 binary  = 10101010
+            # 101010101 len = 9  and 10101010 len = 8
+        # Example 3:
+            # decimal = 75 binary = 1001011
+            # if n > 1, the answer is 1 more than the number of digits in the binary representation of n/2. 
+            # - > 75 // 2 = 37
+            # decimal = 37 binary  = 100101
+            # 1001011 len = 7  and 100101 len = 6          
+    decimal_number = 75
     print(getbinary(decimal_number))
+    decimal_number = 32
+    print(getbinary(decimal_number))
+    
+    # c) 
+    # T(n) = 1 + T(n / 2) + 1              n = 6
+    #      = 1 + [1 + T(n / 2)] + 1        count = 1
+    #      = 2 + [1 + T(n / 4)] + 1        count = 2
+    #      = 3 + [1 + T(n / 6)] + 1        count = 3
+    #      = k + T(n / 2) + 1
+    #      = n + T(0)
+    #      = n + O
+    #      O(n)
+    
+    # d)
+    # T(n) = 1 + T(n / 2) + 1     n = 4
+    # T(n) = 1 + T(n / 2) + n^0
+    # a =  1, b = 3 , d = 0
+    # 1 = 3^0 = 1
+    # case 2: a = b^d  then T(n) = 0(n^d * lg(n))
+    # Big Oh ->  O(n^d * lg(n))
 
 main()
