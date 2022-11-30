da24d890c507c6765d1e4b72bd235480ae4beaed
yannelly25 <yannellym@gmail.com>
2022-11-29 19:51:01 -0500

    first draft module 6

--- algosAndDSA/module6/main.py
+++ algosAndDSA/module6/main.py
@@ -26,6 +26,28 @@ def main():
         # it serves as a counter for the function
         return getbinary(number // 2) + 1  
     
+    # 2) a) Write python code for a recursive algorithm that will calculate the sum of the squares of the
+    # positive integers 12 + 22 + 32 + ... + 𝑛𝑛2 when supplied with a positive integer n.
+    # The logic of the recursive algorithm should be something like: 
+    # if n = 1, the answer is 1; 
+    # if n > 1, the answer is (the sum of the squares of the integers from 1 to n-1) + 𝑛𝑛2.  
+    # Do not find a closed form formula for the summation; make your algorithm do all the arithmetic. 
+    # Your function should have only one input parameter – a positive integer n. It should return the 
+    # sum of the sqaures using a return statement. There should be no print statements in the function. 
+   
+    def getSum_squares(digit):
+        '''
+        Function: calculates the sum of the squares of the positive integers 12 + 22 + 32 + ... + 𝑛𝑛2 when supplied with a positive integer n.
+        Input: A positive integer number.
+        Output: A number representing the sum of the squares of the positive integers
+        '''
+        # Base case for the function
+        if digit == 0:   # if the number is 0, returns 0
+            return 0
+        # this goes through each time and does the following:
+        # f(x) = 1^2 + 2^2 + 3^2 + 4^2 + 5^2  ... (n-1)^2 + n^2
+        # f(n) = f(n-1) + n^2
+        return getSum_squares(digit - 1) + digit * digit
    
     # functions to be executed
     
@@ -52,12 +74,12 @@ def main():
             # - > 75 // 2 = 37
             # decimal = 37 binary  = 100101
             # 1001011 len = 7  and 100101 len = 6          
-    decimal_number = 75
-    print(getbinary(decimal_number))
-    decimal_number = 32
-    print(getbinary(decimal_number))
+    # decimal_number = 75
+    # print(getbinary(decimal_number))
+    # decimal_number = 32
+    # print(getbinary(decimal_number))
     
-    # c) 
+    # 1) c) 
     # T(n) = 1 + T(n / 2) + 1              n = 6
     #      = 1 + [1 + T(n / 2)] + 1        count = 1
     #      = 2 + [1 + T(n / 4)] + 1        count = 2
@@ -67,12 +89,37 @@ def main():
     #      = n + O
     #      O(n)
     
-    # d)
+    # 1) d)
     # T(n) = 1 + T(n / 2) + 1     n = 4
     # T(n) = 1 + T(n / 2) + n^0
     # a =  1, b = 3 , d = 0
     # 1 = 3^0 = 1
     # case 2: a = b^d  then T(n) = 0(n^d * lg(n))
     # Big Oh ->  O(n^d * lg(n))
+    
+    # 2) b) 
+    # Example 1:
+      # n = 5
+      # 1^2 + 2^2 + 3^2 + 4^2 + 5^2  = 55
+    # Example 2:
+      # n = 10
+      # 1^2 + 2^2 + 3^2 + 4^2 + 5^2 + 6^2 + 7^2 + 8^2 + 9^2 + 10^2 = 385
+    n = 1
+    print(getSum_squares(n))
+    n = 10
+    print(getSum_squares(n))
+    
+    # 2) c)
+    # T(n) = 2 + T(n - 1) + n * n      
+    #      
+    
+    # 2) d) 
+    # back substitution method 
+    # T(n) = 2 + T(n - 1) + n * n 
+    #      = 2[1 (n-1)] + T(n-2)]   
+    #      = 2[1 (n-1) + [T(n-2) +  (n-3)]  
+    #      = 2[1 (n-1) + (n-2) + (n-3)
+    #      = 2 * n(n +1) / 2
+    #      =  O(n)^2
 
 main()
