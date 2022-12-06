9b530a704ede79c4968e5367d4ddb84d6295ec59
yannelly25 <yannellym@gmail.com>
2022-12-06 16:49:19 -0500

    final draft

--- algosAndDSA/module7/main.py
+++ algosAndDSA/module7/main.py
@@ -1,20 +1,28 @@
 def main():
+    # problem 1
     A1, n1 = [12,  23,  37,  45,  63,  82,  47,  75,  91,  88,  102], 11           # 60.45454545454545 -> rounded to nearest whole num = 60
     A2, n2 = [-1.7,  6.5,  8.2,  0.0,  4.7,  6.3,  9.5,  12.2,  37.9,  53.2], 10   # 13.68 -> rounded to nearest whole num = 14
-    
     # print(mean(A1, n1))
     # print(mean(A2, n2))
     
-    A = [100, 87, 85, 80, 72, 67, 55, 50, 48, 42, 40, 31, 25, 22, 18]
+    # problem 2
+    A = [100, 87, 85, 80, 72, 67, 55, 50, 48, 42, 40, 31, 25, 22, 18] 
     k1, k2 , k3, k4 = 87, 48, 33, 10
     start, end = 0, len(A)-1
+    # print(binarySearch(A, start, end, k1))        # => [100, 87]
+    # print(binarySearch(A, start, end, k2))        # => [48]
+    # print(binarySearch(A, start, end, k3))        # =>  None
+    # print(binarySearch(A, start, end, k4))        # =>  None
     
-    # print(binarySearch(A, start, end, k1))
-    # print(binarySearch(A, start, end, k2))
-    # print(binarySearch(A, start, end, k3))
-    # print(binarySearch(A, start, end, k4))
-    
-
+    # problem 3
+    l1, l2 = 2468, 1357              
+    m1, m2 = 111, 378                
+    n1, n2 = 123456789, 987654321 
+    # print(GDC(30, 70))       # -> 10
+    # print(GDC(60, 24))       # -> 12
+    # print(GDC(l1, l2))       # -> 1
+    # print(GDC(m1, m2))       # -> 3
+    # print(GDC(n1, n2))       # -> 9
 
 
 
@@ -29,10 +37,11 @@ Input: A(array of numbers), n(integer indicating how many of the array values sh
 Output: an Int, calculated value of the mean.
 '''    
 def mean(A, n):
-    if n == 1:
-        return A[n-1]
+    if n == 1:         # if the length of the array is 1
+        return A[n-1]  # just return the only digit in the array
     else:
-        # ( (mean(A, n - 1) * (n - 1) + A[n - 1]) /n)
+        # else, return the result of n/1 times the result of calling the function again with n-1
+        # then add this result to 1/n times n-1
         return  round( ((n-1)/n) * mean(A, n-1)  + (1/n) * A[n-1])
 
 
@@ -57,9 +66,9 @@ def binarySearch(A, start, end, k):
         if A[mid] == k:
             return (A[start:mid+1])  # subscript of the one “middle” element that is compared to the searchkey
         elif k > A[mid]:
-            return binarySearch(A, start, mid - 1 , k)
+            return binarySearch(A, start, mid - 1 , k)  # call the function again but make the end equal to mid - 1 so we shorten the sub-array we are searching in 
         else:
-            return binarySearch(A,mid + 1, end,  k)
+            return binarySearch(A,mid + 1, end,  k)    # call the function again but make the start equal to mid + 1 so we shorten the sub-array we are searching in 
 
     
 # 3) A “decrease-by-a variable amount” algorithm - Euclidean Algorithm for Greatest 
@@ -76,10 +85,11 @@ def binarySearch(A, start, end, k):
 # iii) GCD (123456789, 987654321)  
 
 def GDC(m,n):
-    if n ==0:
+    print(f"integers being passed: {m}, {n}")
+    if n ==0:    # if n is 0, just return m 
         return m
     else:
-        return GDC(n, m % n)
+        return GDC(n, m % n)   # m % n produces the remainder after dividing by n 
         
 
 main()
